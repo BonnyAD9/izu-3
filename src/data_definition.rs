dc1e76b634d10dad8c0f1ac168aa4694d373e620
Jakub Antonín Štigler <jakubstig@gmail.com>
2024-04-10 09:10:46 +0200

    Add attribute parsing

--- src/data_definition.rs
+++ src/data_definition.rs
@@ -3,7 +3,7 @@ use std::{
     io::BufRead,
 };
 
-use anyhow::{bail, Error, Result};
+use anyhow::{anyhow, bail, Error, Result};
 
 #[derive(Default)]
 pub struct DataDefinition {
@@ -32,35 +32,61 @@ impl DataDefinition {
         let mut data = DataDefinition::default();
         while let Some(line) = lines.next() {
             let line = line?;
-            match line.as_str() {
+            match line.trim() {
                 "" => {}
-                "attributes {" => data.parse_attributes(&mut lines)?,
-                "classes {" => data.parse_classes(&mut lines)?,
-                "objects {" => data.parse_objects(&mut lines)?,
+                "attributes {" => {
+                    data.parse_with(Self::parse_attribute, &mut lines)?
+                }
+                "classes {" => {
+                    data.parse_with(Self::parse_class, &mut lines)?
+                }
+                "objects {" => {
+                    data.parse_with(Self::parse_object, &mut lines)?
+                }
                 _ => bail!("Unexpected line in input: {line}"),
             }
         }
         todo!()
     }
 
-    fn parse_attributes<I>(&mut self, lines: &mut I) -> Result<()>
+    fn parse_with<F, I>(&mut self, f: F, lines: &mut I) -> Result<()>
     where
         I: Iterator<Item = Result<String>>,
+        F: Fn(&mut Self, &str) -> Result<()>,
     {
+        while let Some(line) = lines.next() {
+            let line = line?;
+            if line == "}" {
+                break;
+            }
+            f(self, &line)?;
+        }
+        Ok(())
+    }
+
+    fn parse_attribute(&mut self, line: &str) -> Result<()> {
+        let mut spl = line.split_ascii_whitespace();
+        let name = spl.next().ok_or(anyhow!("missing attribute name"))?;
+        if spl.next().ok_or(anyhow!("missing :"))? != ":" {
+            bail!("Expected :");
+        }
+        
+        let mut values = HashSet::new();
+        values.extend(spl.map(str::to_string));
+
+        self.attributes.push(Attribute {
+            name: name.to_owned(),
+            values,
+        });
+
+        Ok(())
+    }
+
+    fn parse_class(&mut self, line: &str) -> Result<()> {
         todo!()
     }
 
-    fn parse_classes<I>(&mut self, lines: &mut I) -> Result<()>
-    where
-        I: Iterator<Item = Result<String>>,
-    {
-        todo!()
-    }
-
-    fn parse_objects<I>(&mut self, lines: &mut I) -> Result<()>
-    where
-        I: Iterator<Item = Result<String>>,
-    {
+    fn parse_object(&mut self, line: &str) -> Result<()> {
         todo!()
     }
 }
